cd483b6cb40ce8c1452bb47f047c5fc171f65bcc
Typhaine Le Gales <typhaineLeGales@protontmail.com>
2024-10-29 07:09:34 +0100

    feat: project blueprint structure

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -12,32 +12,22 @@
   </main>
 
   <footer class="footer">
-    <div class="grid">
-      <div class="column" style="--columns: 8">
-        <h2><a href="https://getkirby.com">Made with Kirby</a></h2>
-        <p>
-          Kirby: the file-based CMS that adapts to any project, loved by developers and editors alike
-        </p>
-      </div>
-      <div class="column" style="--columns: 2">
-        <h2>Pages</h2>
-        <ul>
-          <?php foreach ($site->children()->listed() as $example): ?>
-          <li><a href="<?= $example->url() ?>"><?= $example->title()->esc() ?></a></li>
-          <?php endforeach ?>
-        </ul>
-      </div>
-      <div class="column" style="--columns: 2">
-        <h2>Kirby</h2>
-        <ul>
-          <li><a href="https://getkirby.com">Website</a></li>
-          <li><a href="https://getkirby.com/docs">Docs</a></li>
-          <li><a href="https://forum.getkirby.com">Forum</a></li>
-          <li><a href="https://chat.getkirby.com">Chat</a></li>
-          <li><a href="https://github.com/getkirby">GitHub</a></li>
-        </ul>
-      </div>
-    </div>
+    <a class="logo" href="<?= $site->url() ?>">
+      <?= $site->title()->esc() ?>
+    </a>
+    <p>©2024 All rights reserved</p>
+    <ul>
+ 
+        <li>
+          <a href="Home">Home |</a>
+          <a href="About">About |</a>
+          <a href="Donations">Donations |</a>
+          <a href="Contact">Contact |</a>
+          <a href="Projects">Projects |</a>
+          <a href="Articles">Articles</a>
+        </li>
+
+    </ul>
   </footer>
 
   <?= js([
