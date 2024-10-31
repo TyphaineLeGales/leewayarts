d66b4ff12f28bf98e0b93895c018686efd98fba5
Typhaine Le Gales <typhaineLeGales@protontmail.com>
2024-10-31 16:38:49 +0100

    feat: project grid

--- site/snippets/related.php
+++ site/snippets/related.php
@@ -1,16 +1,19 @@
-<h2> Related projects <h2>
-<?php
-$related = $page->related()->toPages();
-if ($related->count() > 0):
-?>
+<div class="project__related middle-layout">
 
-<ul class="project__related">
-    <?php foreach($related as $article): ?>
-        <li>
-            <a href="<?= $article->url() ?>">
-            <?= $article->title() ?>
-            </a>
-        </li>
-    <?php endforeach ?>
-</ul>
-<?php endif ?>
+    <h2> Related projects <h2>
+    <?php
+    $related = $page->related()->toPages();
+    if ($related->count() > 0):
+    ?>
+    
+    <ul>
+        <?php foreach($related as $article): ?>
+            <li>
+                <a href="<?= $article->url() ?>">
+                <?= $article->title() ?>
+                </a>
+            </li>
+        <?php endforeach ?>
+    </ul>
+    <?php endif ?>
+</div>
--- site/templates/project.php
+++ site/templates/project.php
@@ -1,22 +1,29 @@
 <?php snippet('header') ?>
-<h1><?= $page->title() ?></h1>
-<div class="project-thumbnail-wrapper">
-     <!--  CARROUSEL -->
-    <img class="project__thumbnail" src="<?= $page->thumbnail()->toFile()->url() ?>"/>
+<?= css([
+    'assets/css/pagegrid.css',
+  ]) ?>
+<div class="content">
+  <h1 class="project__title"><?= $page->title() ?></h1>
+  <div class="project__images">
+       <!--  CARROUSEL -->
+      <img class="project__thumbnail" src="<?= $page->thumbnail()->toFile()->url() ?>"/>
+  </div>
+   <!--  DESCRIPTION -->
+  <div class="project__text middle-layout"><?= $page->description()->toBlocks() ?></div>
+  <!--  QUOTE -->
+  <quote class='project__quote'><?= $page->quote() ?></quote/>
+  <!--  VIDEO -->
+  <div class="project__video middle-layout">
+    <?= video($page->video()) ?>
+  </div>
+  <!--  PROJECT CREDITS -->
+  <p class="project__credits middle-layout">
+    <?php $credits = $page->credits()->toStructure(); foreach ($credits as $credit): ?> 
+      <span><?= $credit->role() ?></span>  <?=$credit->people()?> |
+    <?php endforeach ?>
+  </p>
+  
+  <!--  RELATED -->
+  <?php snippet('related') ?>
+  <?php snippet('prevnext') ?>
 </div>
- <!--  DESCRIPTION -->
-<p class="project__description"><?= $page->description()->toBlocks() ?></p>
-<!--  QUOTE -->
-<quote class='project__quote'><?= $page->quote() ?></quote/>
-<!--  VIDEO -->
-<?= video($page->video()) ?>
-<!--  PROJECT CREDITS -->
-<p class="project__credits">
-  <?php $credits = $page->credits()->toStructure(); foreach ($credits as $credit): ?> 
-    <span><?= $credit->role() ?></span>  <?=$credit->people()?> |
-  <?php endforeach ?>
-</p>
-
-<!--  RELATED -->
-<?php snippet('related') ?>
-<?php snippet('prevnext') ?>
