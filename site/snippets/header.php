bec989438cc8abb44c99953717550610e35fd35b
Typhaine Le Gales <typhaineLeGales@protonmail.com>
2024-11-08 10:17:34 +0100

    clean header.php

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,20 +1,6 @@
-<?php
-/*
-  Snippets are a great way to store code snippets for reuse
-  or to keep your templates clean.
-
-  This header snippet is reused in all templates.
-  It fetches information from the `site.txt` content file
-  and contains the site navigation.
-
-  More about snippets:
-  https://getkirby.com/docs/guide/templates/snippets
-*/
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <title><?= $site->title()->esc() ?> | <?= $page->title()->esc() ?></title>
@@ -30,15 +16,7 @@
   <link rel="shortcut icon" type="image/x-icon" href="<?= url('favicon.ico') ?>">
 </head>
 <body>
-
   <header class="header">
-    <?php
-    /*
-      We use `$site->url()` to create a link back to the homepage
-      for the logo and `$site->title()` as a temporary logo. You
-      probably want to replace this with an SVG.
-    */
-    ?>
     <a class="logo" href="<?= $site->url() ?>">
       <?= $site->title()->esc() ?>
     </a>
@@ -64,5 +42,4 @@
     </nav>
   </header>
   <div id="menu_overlay"></div>
-
   <main class="main">
