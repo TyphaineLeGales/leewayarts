44c394ada138b145c96a34a48d6a9bd56ffb7eb6
Typhaine Le Gales <typhaineLeGales@protonmail.com>
2024-11-18 14:16:02 +0100

    fix: arrows in prev/next

--- site/snippets/prevnext.php
+++ site/snippets/prevnext.php
@@ -10,14 +10,14 @@
                 <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg" class="arrow-icon-container">
                     <path d="M20 8.79191V11.3329H4.84848L11.7929 18.3207L10 20.1248L0 10.0624L10 0L11.7929 1.80412L4.84848 8.79191H20Z" class="arrow"/>
                 </svg>
-                Previous 
+                <p>Previous</p>
             </a>
         </div>
     <?php endif ?>
     <?php if ($page->hasNextListed()): ?>
         <div class="subNav">
             <a href="<?= $page->nextListed()->url() ?>"> 
-                Next 
+                <p>Next</p>
                 <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg"  class="arrow-icon-container">
                     <path d="M20 8.79191V11.3329H4.84848L11.7929 18.3207L10 20.1248L0 10.0624L10 0L11.7929 1.80412L4.84848 8.79191H20Z" class="arrow left"/>
                 </svg>
--- site/templates/donation.php
+++ site/templates/donation.php
@@ -3,8 +3,18 @@
   <h1 class="grid_row"><?= $page->title() ?></h1>
   <p class="grid_row"> Support our mission to dance by donating monthly or just once. All of your contributions go towards  sustaining our operations, and we are very grateful for the support! </p>
   <div class="grid_row btn_container">
-    <a href="https://www.paypal.com/donate/?hosted_button_id=DAAVE6NTQLJCA" target="_blank">Donate via Paypal</a>
-    <a href="https://tikkie.me/pay/Stichti2884/tLqoAiTnsuAm95MmNc9T8F" target="_blank">Donate with Ideal</a>
+    <a href="https://www.paypal.com/donate/?hosted_button_id=DAAVE6NTQLJCA" target="_blank">
+      Donate via Paypal 
+      <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg" class="arrow-icon-container">
+        <path d="M20 8.79191V11.3329H4.84848L11.7929 18.3207L10 20.1248L0 10.0624L10 0L11.7929 1.80412L4.84848 8.79191H20Z" class="arrow left"/>
+      </svg>
+    </a>
+    <a href="https://tikkie.me/pay/Stichti2884/tLqoAiTnsuAm95MmNc9T8F" target="_blank">
+      Donate with Ideal
+      <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg" class="arrow-icon-container">
+        <path d="M20 8.79191V11.3329H4.84848L11.7929 18.3207L10 20.1248L0 10.0624L10 0L11.7929 1.80412L4.84848 8.79191H20Z" class="arrow left"/>
+      </svg>
+    </a>
   </div>
   <div class="grid_row">
     <?php foreach ($page->description()->toBlocks() as $block): ?>
