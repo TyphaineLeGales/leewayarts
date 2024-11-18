6a6ab8b3fc160939b4065ad691e9747d48e6a340
Typhaine Le Gales <typhaineLeGales@protonmail.com>
2024-11-18 15:22:07 +0100

    fix: prev/next nav in a loop

--- site/snippets/prevnext.php
+++ site/snippets/prevnext.php
@@ -3,26 +3,30 @@
 <?= css([
     'assets/css/snippets/prevNext.css',
   ]) ?>
+
+<?php 
+// Get all listed pages
+$listedPages = $page->siblings()->listed();
+
+// Find the previous and next pages in a circular loop
+$prevPage = $page->hasPrevListed() ? $page->prevListed() : $listedPages->last();
+$nextPage = $page->hasNextListed() ? $page->nextListed() : $listedPages->first();
+?>
 <nav class="project__prevnext">
-    <?php if ($page->hasPrevListed()): ?>
-        <div class="subNav">
-            <a href="<?= $page->prevListed()->url() ?>" > 
-                <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg" class="arrow-icon-container">
-                    <path d="M20 8.79191V11.3329H4.84848L11.7929 18.3207L10 20.1248L0 10.0624L10 0L11.7929 1.80412L4.84848 8.79191H20Z" class="arrow"/>
-                </svg>
-                <p>Previous</p>
-            </a>
-        </div>
-    <?php endif ?>
-    <?php if ($page->hasNextListed()): ?>
-        <div class="subNav">
-            <a href="<?= $page->nextListed()->url() ?>"> 
-                <p>Next</p>
-                <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg"  class="arrow-icon-container">
-                    <path d="M20 8.79191V11.3329H4.84848L11.7929 18.3207L10 20.1248L0 10.0624L10 0L11.7929 1.80412L4.84848 8.79191H20Z" class="arrow left"/>
-                </svg>
-            </a>
-           
-        </div>
-    <?php endif ?>
+    <div class="subNav">
+        <a href="<?= $prevPage->url() ?>"> 
+            <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg" class="arrow-icon-container">
+                <path d="M20 8.79191V11.3329H4.84848L11.7929 18.3207L10 20.1248L0 10.0624L10 0L11.7929 1.80412L4.84848 8.79191H20Z" class="arrow"/>
+            </svg>
+            <p>Previous</p>
+        </a>
+    </div>
+    <div class="subNav">
+        <a href="<?= $nextPage->url() ?>"> 
+            <p>Next</p>
+            <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg"  class="arrow-icon-container">
+                <path d="M20 8.79191V11.3329H4.84848L11.7929 18.3207L10 20.1248L0 10.0624L10 0L11.7929 1.80412L4.84848 8.79191H20Z" class="arrow left"/>
+            </svg>
+        </a>
+    </div>
 </nav>
