77dc43602707a70c3044fc961074d125ff1ae927
Typhaine Le Gales <typhaineLeGales@protonmail.com>
2024-11-22 12:58:37 +0100

    feat: upload pdf

--- site/templates/articles.php
+++ site/templates/articles.php
@@ -5,12 +5,19 @@
   ]) ?>
 <h1><?= $page->title() ?></h1>
 <div class="grid">
-    <?php $index = 0; foreach ($page->articles()->toStructure() as $article): $index++;?>
+    <?php $index = 0; foreach ($page->articles()->toStructure() as $article): $index++; ?>
         <li class="grid__item">
-            <a href="<?= $article->url() ?>" target='_blank'>
-                <div class="grid__item_image" style="background-image: url('<?=  $article->image()->toFile()->url() ?>');"></div>
+            <?php
+            $file = $article->pdf()->toFile();
+            $url = $file 
+                ? $file->url() // Use the file's URL if it exists
+                : ($article->url()->isNotEmpty() ? $article->url() : '#'); // Otherwise, use the external URL, or fallback to '#'
+            $isPdf = $file ? $file->extension() === 'pdf' : false;
+            ?>
+            <a href="<?= $url ?>" <?= $isPdf ? '' : "target='_blank'" ?>>
+                <div class="grid__item_image" style="background-image: url('<?= $article->image()->toFile()->url() ?>');"></div>
             </a>
-            <p><?=  $article->legend()?></p>
+            <p><?= $article->legend() ?></p>
         </li>
     <?php endforeach ?>
 </div>
