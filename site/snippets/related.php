286def30c33de985aa85e5b6880be72d73516216
Typhaine Le Gales <typhaineLeGales@protonmail.com>
2024-11-22 13:07:39 +0100

    fix: conditional display of related pages

--- site/snippets/related.php
+++ site/snippets/related.php
@@ -2,20 +2,20 @@
     'assets/css/snippets/related.css',
   ]) ?>
 
-<div class="related middle-layout">
-    <h3> Related pages </h3>
-    <ul class="related__link">
-        <?php
-        $related = $page->related()->toPages();
-        if ($related->count() > 0):
-        ?>
-        <?php foreach($related as $article): ?>
-            <li>
-                <a href="<?= $article->url() ?>">
-                <?= $article->title() ?>
-                </a>
-            </li>
-        <?php endforeach ?>
-    </ul>
-    <?php endif ?>
-</div>
+<?php
+$related = $page->related()->toPages();
+if ($related->count() > 0):
+?>
+    <div class="related middle-layout">
+        <h3> Related pages </h3>
+        <ul class="related__link">
+            <?php foreach($related as $article): ?>
+                <li>
+                    <a href="<?= $article->url() ?>">
+                    <?= $article->title() ?>
+                    </a>
+                </li>
+            <?php endforeach ?>
+        </ul>
+    </div>
+<?php endif ?>
